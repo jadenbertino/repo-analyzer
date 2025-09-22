4174ab44ad9155cfdfc4a646c0607152ccd899a1
Jaden Bertino <jaden@bertinofamily.com>
2025-09-22 15:51:31 -0700

    move loggers

--- src/lib/clients/browser.ts
+++ src/lib/clients/browser.ts
@@ -1,9 +1,35 @@
-import { CLIENT_ENV } from '@/env/client'
+import type { LogLevel } from '@/env/client'
+import { CLIENT_ENV, LOG_LEVELS } from '@/env/client'
 import { createClient } from '@supabase/supabase-js'
+import { serialize } from '../utils'
 
 const supabase = createClient(
   CLIENT_ENV.SUPABASE_URL,
   CLIENT_ENV.SUPABASE_PUBLIC_KEY,
 )
+
+type LogMethod = (message: string, data?: object | unknown) => void
+
+const clientLogger: Record<LogLevel, LogMethod> = {
+  debug: createLogMethod('debug'),
+  info: createLogMethod('info'),
+  warn: createLogMethod('warn'),
+  error: createLogMethod('error'),
+}
+
+function createLogMethod(level: LogLevel): LogMethod {
+  return (message: string, data?: object | unknown) => {
+    const shouldLog =
+      LOG_LEVELS.indexOf(level) >= LOG_LEVELS.indexOf(CLIENT_ENV.LOG_LEVEL)
+    if (shouldLog) {
+      console.log(
+        `[${level.toUpperCase()}] ${message}`,
+        data ? serialize(data) : '',
+      )
+    }
+  }
+}
+
+export { clientLogger }
 
 export { supabase }
--- src/lib/clients/server.ts
+++ src/lib/clients/server.ts
@@ -1,3 +1,29 @@
-import serverLogger from '../logs/server';
+import pino from 'pino'
 
-export { serverLogger }
+// Factory to create wrapped logger with consistent API
+function createLogger(
+  basePinoLogger: pino.Logger,
+): Record<pino.Level, (message: string, data?: object | unknown) => void> {
+  return {
+    trace: createLogMethod(basePinoLogger, 'trace'),
+    debug: createLogMethod(basePinoLogger, 'debug'),
+    info: createLogMethod(basePinoLogger, 'info'),
+    warn: createLogMethod(basePinoLogger, 'warn'),
+    error: createLogMethod(basePinoLogger, 'error'),
+    fatal: createLogMethod(basePinoLogger, 'fatal'),
+  }
+}
+
+function createLogMethod(basePinoLogger: pino.Logger, level: pino.Level) {
+  return (message: string, data?: object | unknown) => {
+    if (data && typeof data === 'object') {
+      basePinoLogger[level](data, message)
+    } else {
+      basePinoLogger[level](message)
+    }
+  }
+}
+
+const serverLogger = createLogger(pino())
+
+export default serverLogger
