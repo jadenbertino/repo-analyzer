aa66e005ee9432e3c7e2a3b8f6c11b0392ca13cf
Jaden Bertino <jaden@bertinofamily.com>
2025-09-22 15:56:14 -0700

    improve loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,6 +1,8 @@
-const LoadingSpinner = () => {
+import { cn } from '@/lib/utils'
+
+const LoadingSpinner = ({ className }: { className?: string }) => {
   return (
-    <span className='truncate'>
+    <span className={cn('truncate', className)}>
       <div className='w-full h-full flex items-center justify-center'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
@@ -9,9 +11,9 @@ const LoadingSpinner = () => {
           viewBox='0 0 24 24'
           fill='none'
           stroke='currentColor'
-          stroke-width='2'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
           className='lucide lucide-loader-circle animate-spin text-foreground-lighter'
         >
           <path d='M21 12a9 9 0 1 1-6.219-8.56'></path>
