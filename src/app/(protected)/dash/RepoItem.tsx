ca033d7b7d50bd65194efc54a72b77a6fcf18044
Jaden Bertino <jaden@bertinofamily.com>
2025-09-22 16:43:37 -0700

    add generic repos list

---
+++ src/app/(protected)/dash/RepoItem.tsx
@@ -0,0 +1,120 @@
+import { cn } from '@/lib/utils'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { EllipsisVertical } from 'lucide-react'
+import { useMemo } from 'react'
+
+export type RepoStatus =
+  | 'Importing'
+  | 'Reviewing'
+  | 'Bugs'
+  | 'Healthy'
+  | 'Error'
+  | 'Archived'
+
+export interface RepoItemType {
+  id: number
+  url: string
+  owner: string
+  name: string
+  owner_name: string
+  user_id: string
+  status: RepoStatus
+  updated_at: string
+}
+
+export default function RepoItem({ repo }: { repo: RepoItemType }) {
+  return (
+    <div className='flex items-center justify-between gap-x-6 py-5'>
+      <div className='min-w-0'>
+        <div className='flex items-start gap-x-3'>
+          <p className='text-sm/6 font-semibold text-gray-900 dark:text-white'>
+            {repo.name}
+          </p>
+          <RepoStatusBadge status={repo.status} />
+        </div>
+        <div className='mt-1 flex items-center gap-x-2 text-xs/5 text-gray-500 dark:text-gray-400'>
+          <p className='whitespace-nowrap'>
+            Updated at{' '}
+            <time dateTime={repo.updated_at}>
+              {new Date(repo.updated_at).toLocaleDateString()}
+            </time>
+          </p>
+          <svg
+            viewBox='0 0 2 2'
+            className='size-0.5 fill-current'
+          >
+            <circle
+              r={1}
+              cx={1}
+              cy={1}
+            />
+          </svg>
+          <p className='truncate'>Owner: {repo.owner_name}</p>
+        </div>
+      </div>
+      <div className='flex flex-none items-center gap-x-4'>
+        <a
+          href={repo.url}
+          className='hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50 sm:block dark:bg-white/10 dark:text-white dark:shadow-none dark:inset-ring-white/5 dark:hover:bg-white/20'
+        >
+          View repo<span className='sr-only'>, {repo.name}</span>
+        </a>
+        <Menu
+          as='div'
+          className='relative flex-none'
+        >
+          <MenuButton className='relative flex items-center justify-center p-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white cursor-pointer'>
+            <span className='sr-only'>Open options</span>
+            <EllipsisVertical
+              aria-hidden='true'
+              className='size-5'
+            />
+          </MenuButton>
+          <MenuItems
+            transition
+            className='absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg outline-1 outline-gray-900/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'
+          >
+            <MenuItem as='div'>
+              <a
+                href='#'
+                className='block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden dark:text-white dark:data-focus:bg-white/5'
+              >
+                Delete<span className='sr-only'>, {repo.name}</span>
+              </a>
+            </MenuItem>
+          </MenuItems>
+        </Menu>
+      </div>
+    </div>
+  )
+}
+
+function RepoStatusBadge({ status }: { status: RepoStatus }) {
+  const statusStyles: Record<RepoStatus, string> = useMemo(
+    () => ({
+      Importing:
+        'bg-blue-50 text-blue-700 inset-ring-blue-600/20 dark:bg-blue-400/10 dark:text-blue-400 dark:inset-ring-blue-500/20',
+      Reviewing:
+        'bg-orange-50 text-orange-700 inset-ring-orange-600/20 dark:bg-orange-400/10 dark:text-orange-400 dark:inset-ring-orange-500/20',
+      Bugs: 'bg-red-50 text-red-700 inset-ring-red-600/20 dark:bg-red-400/10 dark:text-red-400 dark:inset-ring-red-500/20',
+      Healthy:
+        'bg-green-50 text-green-700 inset-ring-green-600/20 dark:bg-green-400/10 dark:text-green-400 dark:inset-ring-green-500/20',
+      Error:
+        'bg-red-50 text-red-700 inset-ring-red-600/20 dark:bg-red-400/10 dark:text-red-400 dark:inset-ring-red-500/20',
+      Archived:
+        'bg-yellow-50 text-yellow-800 inset-ring-yellow-600/20 dark:bg-yellow-400/10 dark:text-yellow-500 dark:inset-ring-yellow-400/20',
+    }),
+    [],
+  )
+
+  return (
+    <p
+      className={cn(
+        'mt-0.5 rounded-md px-2 py-0.5 text-xs font-medium inset-ring',
+        statusStyles[status],
+      )}
+    >
+      {status}
+    </p>
+  )
+}
--- src/app/(protected)/dash/page.tsx
+++ src/app/(protected)/dash/page.tsx
@@ -1,12 +1,80 @@
 import { Nav } from './Nav'
+import RepoItem, { RepoItemType } from './RepoItem'
 
 const DashPage = () => {
   return (
     <div>
       <Nav title='Repos' />
-      <main>repos list here</main>
+      <main>
+        <ul
+          role='list'
+          className='divide-y divide-gray-100 dark:divide-white/5'
+        >
+          {projects.map((project) => (
+            <li key={project.id}>
+              <RepoItem
+                key={project.id}
+                repo={project}
+              />
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   )
 }
+
+const projects: RepoItemType[] = [
+  {
+    id: 1,
+    url: 'https://github.com/acme/graphql-api',
+    owner: 'acme',
+    name: 'GraphQL API',
+    owner_name: 'Leslie Alexander',
+    user_id: 'user_123',
+    status: 'Healthy',
+    updated_at: '2023-03-17T00:00Z',
+  },
+  {
+    id: 2,
+    url: 'https://github.com/acme/benefits-plan',
+    owner: 'acme',
+    name: 'New benefits plan',
+    owner_name: 'Leslie Alexander',
+    user_id: 'user_123',
+    status: 'Reviewing',
+    updated_at: '2023-05-05T00:00Z',
+  },
+  {
+    id: 3,
+    url: 'https://github.com/acme/onboarding-emails',
+    owner: 'acme',
+    name: 'Onboarding emails',
+    owner_name: 'Courtney Henry',
+    user_id: 'user_456',
+    status: 'Importing',
+    updated_at: '2023-05-25T00:00Z',
+  },
+  {
+    id: 4,
+    url: 'https://github.com/acme/ios-app',
+    owner: 'acme',
+    name: 'iOS app',
+    owner_name: 'Leonard Krasner',
+    user_id: 'user_789',
+    status: 'Bugs',
+    updated_at: '2023-06-07T00:00Z',
+  },
+  {
+    id: 5,
+    url: 'https://github.com/acme/marketing-site',
+    owner: 'acme',
+    name: 'Marketing site redesign',
+    owner_name: 'Courtney Henry',
+    user_id: 'user_456',
+    status: 'Archived',
+    updated_at: '2023-06-10T00:00Z',
+  },
+]
 
 export default DashPage
