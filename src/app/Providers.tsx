519d70d8288d923bd9b3813d8d3b828ddf124a0f
Jaden Bertino <jaden@bertinofamily.com>
2025-09-22 16:52:42 -0700

    install tanstack query

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,10 +1,35 @@
 'use client'
 
 import { AuthProvider } from '@/context/Auth'
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactNode } from 'react'
 
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    // onError: (error) => {
+    // Sentry.captureException(error)
+    // },
+  }),
+  mutationCache: new MutationCache({
+    // onError: (error) => {
+    // Sentry.captureException(error)
+    // },
+  }),
+})
+
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <AuthProvider>{children}</AuthProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{children}</AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export { Providers }
